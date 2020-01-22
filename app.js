81d6d2580a63f11adfb5638c3cd96fdc95de794e
Jesiel Viana <jesiel.analista@gmail.com>
2020-01-22 16:02:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 conectaMongoDB();
-
-app.listen(3000, () => console.log('App online...'));
+const PORTA = process.env.PORT || 3000
+app.listen(PORTA, () => console.log('App online...'));
